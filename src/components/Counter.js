79b2fdf046d921daab02bd12832a79d984178190
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-02 10:06:14 -0300

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function Counter(props){
+
+    const [count, setCount] = useState(5)
+
+    return(<div>
+        <div>{count}</div>
+        <button onClick={()=>{setCount(prev=> prev - 1)}}>-</button>
+        <button onClick={()=>{setCount(prev=> prev + 1)}}>+</button>
+
+    </div>)
+}
+
+export default Counter;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(props){
+    return(<div className="c-header">
+        <h3>Header</h3>
+        <div>0</div>
+    </div>)
+}
+
+export default Header;
